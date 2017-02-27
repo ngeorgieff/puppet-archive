65e487ccbd264eaf25f2a5be13d31102fcc0176d
Nikolay Georgieff <ngeorgieff@it.ucla.edu>
2017-02-27 14:01:17 -0800

    Testing

--- examples/artifactory.pp
+++ examples/artifactory.pp
@@ -1,5 +1,3 @@
-notice(artifactory_sha1('http://bit.ly/1Tfk4vQ'))
-
 archive::artifactory { '/tmp/logo.png':
   url   => 'https://repo.jfrog.org/artifactory/distributions/images/Artifactory_120x75.png',
   owner => 'root',
